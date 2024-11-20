0fc2e677b10c003099cd3f72a21037707a63bde7
ranruwini <53249186+ranruwini@users.noreply.github.com>
2024-11-20 16:02:51 +0530

    Update Website2.py

--- Website2.py
+++ Website2.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 for i in range(1,5):
     html_text = requests.get(f'https://reserved767.rssing.com/chan-65241722/all_p{i}.html').text
 
     soup = BeautifulSoup(html_text, 'lxml')
+    
+    data =[]
+    
     Obituaries = soup.find_all('header', class_='cs-post-single-title')
 
     for obituary in Obituaries:
@@ -15,3 +19,9 @@ for i in range(1,5):
         Obituary : {obituary_date}
         Published Date: {published_datetime}
         ''')
+        
+        data.append([published_datetime,obituary_date])
+        
+# Save DataFrame to CSV
+df = pd.DataFrame(data, columns=['Obituary', 'Published Date'])
+df.to_csv('website2_scraped_data.csv', index=False)
